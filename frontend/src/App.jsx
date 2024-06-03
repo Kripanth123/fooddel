49e7d422dbfef4180cdbaa79659bd53130690854
Kripanth123 <kripanthmadhav2002@gmail.com>
2024-06-03 22:04:47 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './componets/Navbar/navbar'
+
+function App() {
+  return (
+    <div className='app'>
+      <Navbar/>
+     
+
+    </div>
+  )
+}
+
+export default App
